337414506d4dfd2b98d708c0013fec1336ce22cd
Pratyush Das <reikdas@gmail.com>
2024-06-25 22:27:38 +0000

    Benchmark real world matrices 10 times

--- bench_manualvbr.py
+++ bench_manualvbr.py
@@ -5,6 +5,8 @@ import pathlib
 FILEPATH = pathlib.Path(__file__).resolve().parent
 BASE_PATH = os.path.join(FILEPATH, "..")
 
+BENCHMARK_FREQ = 10
+
 if __name__ == "__main__":
     benchlist = ["bench_inspector", "bench_executor"]
     for benchfile in benchlist:
@@ -16,7 +18,7 @@ if __name__ == "__main__":
                     assert(filename.endswith(".mtx"))
                     f.write(f"{filename[:-4]}")
                     subprocess.call([f"{BASE_PATH}/partially-strided-codelet/build/DDT", "-m", os.path.join(VBR_PATH, filename), "-n", "SPMV", "-s", "CSR", "--"+benchfile, "-t", str(threads)], stdout=subprocess.PIPE)
-                    for i in range(5):
+                    for i in range(BENCHMARK_FREQ):
                         print(f"{i+1}th iteration")
                         output = subprocess.run([f"{BASE_PATH}/partially-strided-codelet/build/DDT", "-m", os.path.join(VBR_PATH, filename), "-n", "SPMV", "-s", "CSR", "--"+benchfile, "-t", str(threads)], capture_output=True)
                         f.write(","+output.stdout.decode("utf-8")[:-1])
